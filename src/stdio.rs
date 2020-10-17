f55654ac1b37000c76c8241b45dae944846defe2
Robin KAY <komadori@gekkou.co.uk>
2020-10-17 23:39:00 +0100

    Remove Read/WriteLine traits because they offer no better locking than using Stdin/out directly.

--- src/stdio.rs
+++ src/stdio.rs
@@ -2,64 +2,15 @@ use crate::error::{Error, Result};
 use crate::prompt::{
   PromptRequester, PromptResponder, ReportKind, RequestKind,
 };
-use std::fmt::Arguments;
-use std::io;
 use std::io::prelude::*;
 use std::io::{stdin, stdout, Stdin, Stdout};
 
-/// Prompt which reads and writes via `ReadLine` and `WriteLine` traits.
+/// Prompt which reads and writes via `std::io` traits.
 pub struct ReadWritePrompt<R, W> {
   read: R,
   write: W,
   is_interactive: bool,
   level: usize,
-}
-
-/// Wrapper which implements `ReadLine` and `WriteLine` over `BufRead` and
-/// `Write`.
-pub struct StdLine<T>(T);
-
-/// Trait for reading lines.
-pub trait ReadLine {
-  fn read_line(&mut self, buf: &mut String) -> io::Result<usize>;
-}
-
-impl<T: BufRead> ReadLine for StdLine<T> {
-  fn read_line(&mut self, buf: &mut String) -> io::Result<usize> {
-    self.0.read_line(buf)
-  }
-}
-
-impl ReadLine for Stdin {
-  fn read_line(&mut self, buf: &mut String) -> io::Result<usize> {
-    self.lock().read_line(buf)
-  }
-}
-
-/// Trait for writing lines.
-pub trait WriteLine {
-  fn write_fmt(&mut self, fmt: Arguments<'_>) -> io::Result<()>;
-  fn flush(&mut self) -> io::Result<()>;
-}
-
-impl<T: Write> WriteLine for StdLine<T> {
-  fn write_fmt(&mut self, fmt: Arguments<'_>) -> io::Result<()> {
-    self.0.write_fmt(fmt)
-  }
-
-  fn flush(&mut self) -> io::Result<()> {
-    self.0.flush()
-  }
-}
-
-impl WriteLine for Stdout {
-  fn write_fmt(&mut self, fmt: Arguments<'_>) -> io::Result<()> {
-    self.lock().write_fmt(fmt)
-  }
-
-  fn flush(&mut self) -> io::Result<()> {
-    self.lock().flush()
-  }
 }
 
 impl<'a> ReadWritePrompt<Stdin, Stdout> {
@@ -73,22 +24,22 @@ impl<'a> ReadWritePrompt<Stdin, Stdout> {
   }
 }
 
-impl<R: BufRead, W: Write> ReadWritePrompt<StdLine<R>, StdLine<W>> {
+impl<R: BufRead, W: Write> ReadWritePrompt<R, W> {
   pub fn new_requester(read: R, write: W, is_interactive: bool) -> Self {
     ReadWritePrompt {
-      read: StdLine(read),
-      write: StdLine(write),
+      read,
+      write,
       is_interactive,
       level: 0,
     }
   }
 }
 
-impl<W: Write> ReadWritePrompt<(), StdLine<W>> {
+impl<W: Write> ReadWritePrompt<(), W> {
   pub fn new_responder(write: W) -> Self {
     ReadWritePrompt {
       read: (),
-      write: StdLine(write),
+      write,
       is_interactive: false,
       level: 0,
     }
@@ -110,7 +61,7 @@ fn lift_result<T, E: std::error::Error>(
   }
 }
 
-impl<'a, R, W: WriteLine> PromptResponder for ReadWritePrompt<R, W> {
+impl<'a, R, W: Write> PromptResponder for ReadWritePrompt<R, W> {
   fn begin_scope(&mut self, name: &str, _size: Option<usize>) -> Result<()> {
     lift_result(writeln!(
       self.write,
@@ -152,7 +103,7 @@ impl<'a, R, W: WriteLine> PromptResponder for ReadWritePrompt<R, W> {
   }
 }
 
-impl<'a, R: ReadLine, W: WriteLine> PromptRequester for ReadWritePrompt<R, W> {
+impl<'a, R: BufRead, W: Write> PromptRequester for ReadWritePrompt<R, W> {
   fn is_interactive(&self) -> bool {
     self.is_interactive
   }
